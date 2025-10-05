41c3dc9f9a9b7db712a3cc2ffaa33879baab4567
pertsevpy <pertsevpy@users.noreply.github.com>
2025-10-05 09:35:30 +0300

    Init CI/CD
    Update test
    
    add conftest.py
    
    Update CI/CD excludes

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+"""автоматически настраивать pytest"""
+
+import sys
+import os
+
+# Добавляем корень проекта в sys.path
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
